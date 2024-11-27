2bdd7e1fbc86c29a9b63cd1e8147abaa12dfd5c1
Paulo Ximenes <pauloxmj@gmail.com>
2024-11-27 20:17:29 -0300

    Minor changes

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,29 +2,34 @@ import { useState } from "react"
 import styles from "./Main.module.css"
 
 export default function Main() {
-    //Initializing state for ingredients
+    // Initializing state for ingredients
     const [ingredients, setIngredients] = useState([]);
 
-    //Mapping ingredients to li items
+    // Mapping ingredients to li items
     const ingredientsListItems = ingredients.map(ingredient => (
         <li key={ingredient}>{ingredient}</li>
     ));
 
+    // React 18 handling of form data:
     function handleSubmit(event) {
         event.preventDefault(); // Prevents page refresh when submitting form
-
         const formData = new FormData(event.currentTarget);
         const newIngredient = formData.get("ingredient").trim(); //Get form data and trim input from any whitespaces
-
-        //Snippet to only add ingredients when form is not empty
+        // Statement to only add ingredients when form is not empty
         if (newIngredient) {
             setIngredients(prevIngredients => [...prevIngredients, newIngredient]);
         }
-
         // Clear the input field after adding ingredient
-        event.currentTarget.reset();
-
+        event.currentTarget.reset()
     }
+
+    // React 19 handling of form data:
+    // function addIngredient(formData) {
+    //     const newIngredient = formData.get("ingredient")
+    //     setIngredients(prevIngredients => [...prevIngredients, newIngredient])
+    // }
+    // <main>
+    //  <form action={addIngredient} className={styles.addIngredientForm}>
 
     return(
         <main>
